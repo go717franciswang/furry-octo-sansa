e4c38afe8a9e5378fd8340c3e19522fec6810a7b
Francis Wang <go717franciswang@hotmail.com>
2014-01-01 11:53:29 -0800

    Complete ex3

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,14 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = (-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-sigmoid(X*theta))) / m + ...
+    lambda/(2*m) * (theta(2:end)'*theta(2:end));
 
-
-
-
-
-
-
-
+grad = X'*(sigmoid(X*theta)-y) / m;
+grad(2:end) = grad(2:end) + lambda/m * theta(2:end);
 
 % =============================================================
 
